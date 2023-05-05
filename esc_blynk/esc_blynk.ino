016478c1f84a03a3bcd9f2bb730e5d8b5bcf0b94
datdt182411 <97877886+datdt182411@users.noreply.github.com>
2023-05-05 14:34:51 +0700

    Update esc_blynk.ino

--- esc_blynk/esc_blynk.ino
+++ esc_blynk/esc_blynk.ino
@@ -73,24 +73,24 @@ Servo *sv[4]{
 };
 //////
 
-bool blinkState = false;                                                                //  ???
-WidgetTerminal terminal(V3);                                                            //  ???
+bool blinkState = false;                                                                
+WidgetTerminal terminal(V3);                                                            
 // MPU control/status vars
-bool dmpReady = false;   // set true if DMP init was successful
-uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
-uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;      // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64];  // FIFO storage buffer
+bool dmpReady = false;   
+uint8_t mpuIntStatus;    
+uint8_t devStatus;      
+uint16_t packetSize;     
+uint16_t fifoCount;      
+uint8_t fifoBuffer[64];  
 unsigned int esc_value[4];
 // orientation/motion vars
-Quaternion q;         // [w, x, y, z]         quaternion container
-VectorInt16 aa;       // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;  // [x, y, z]            gravity vector
-float euler[3];       // [psi, theta, phi]    Euler angle container
-float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion q;         
+VectorInt16 aa;       
+VectorInt16 aaReal;   
+VectorInt16 aaWorld;  
+VectorFloat gravity;  
+float euler[3];       
+float ypr[3];         
 double input_pid[3];
 double ypr_sp[3] = { 0, 0, 0 };
 double setpoint_set[3] = { 0, 0, 0 };
@@ -370,7 +370,7 @@ void calculate_pid() {
     input_pid[pitch_] = rad_to_deg(ypr[pitch_]);
     input_pid[roll_] = rad_to_deg(ypr[roll_]);
   }
-  /// 2 mode : goc va toc do goc phan biet bang throttle>1500?goc:tocdo // tocdo -> -90->90 goc: -15->15 . yaw -> -45->45
+  
   pid_pitch.Compute();
   pid_roll.Compute();
   pid_yaw.Compute();
